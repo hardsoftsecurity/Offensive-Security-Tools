32cdfa15115272930bce6c8685ff089f8ea75045
hardsoftsecurity <daviddemayamerras@gmail.com>
2024-10-18 20:24:55 +0200

    Adding new AD tools and organization.

--- CustomScripts/uncommonWinPorts.ps1
+++ CustomScripts/uncommonWinPorts.ps1
@@ -18,13 +18,15 @@ $commonPorts = @(
 # Obtener la lista de puertos a la escucha
 $portsListening = Get-NetTCPConnection -State Listen
 
-# Mostrar los puertos y marcar los inusuales
+# Mostrar los puertos y marcar los inusuales con solo propiedades necesarias, incluyendo el nombre del proceso
 $portsListening | ForEach-Object {
     $port = $_.LocalPort
-    $info = $_
+    $procId = $_.OwningProcess
+    $processName = (Get-Process -Id $procId).ProcessName  # Obtener el nombre del proceso
+
     if ($commonPorts -contains $port) {
-        Write-Output "$info"
+        Write-Output "Port: $($port), Process: $processName, Local Address: $($_.LocalAddress), Remote Address: $($_.RemoteAddress), State: $($_.State)"
     } else {
-        Write-Output "$info --> X (Inusual)"
+        Write-Output "Port: $($port), Process: $processName, Local Address: $($_.LocalAddress), Remote Address: $($_.RemoteAddress), State: $($_.State) --> X (Inusual)"
     }
 }
